b6fe802640e97b799541e9fe4f7e4e215dd9eb17
wevans02 <wevans02@uoguelph.ca>
2025-03-08 16:09:07 -0500

    Update Reasons_Not_In_Job_Market_Data_Feilds.py
    Updated version of Reasons_Not_In_Job_Market.py

--- GroupAssignment/pythonFiles/Reasons_Not_In_Job_Market_Data_Feilds.py
+++ GroupAssignment/pythonFiles/Reasons_Not_In_Job_Market_Data_Feilds.py
@@ -1,15 +1,14 @@
-
 #This file is to collect the needed data feilds from each data file
 import sys
 import csv
 def main(argv):
     #Taking in one data file at a time
-    if len(argv) != 3:
+    if len(argv) != 2:
         print("*Error* Wrong number of expected arguments", file = sys.stderr())
         sys.exit(1)
 
     #Collecting the data file
-    dataFile = argv[1]
+    dataFile = "../original_data_files/Reasons_Not_Looking_For_Work"
     try:
         dataFile_fh = open(dataFile, encoding = "utf-8-sig")
     except TypeError:
@@ -21,6 +20,15 @@ def main(argv):
     for rowDataFeilds in dataFile_reader:
         REF_DATE = rowDataFeilds[0]    
         GEO = rowDataFeilds[1]
+        reason = rowDataFeilds[4]
+        estimates = rowDataFeilds[5]
+        UOM = rowDataFeilds[6]
+        value = rowDataFeilds[12]
+
+        if (reason == "Total, not in the labour force" or reason == "Wanted work, reason - illness" or reason == "Not in the labour force and did not want work or not available"):
+            if (estimates == "Number of persons"):
+                if (UOM == "Number"):
+                    print("%s,%s,%s,%s,%s,%s" % (REF_DATE, GEO, reason, estimates, UOM, value))
 
 
 main(sys.argv)
