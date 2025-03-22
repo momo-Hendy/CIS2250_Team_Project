217fe820fd93813da01a1109b47b1525206d8fa5
Indigo Asher <iasher@uoguelph.ca>
2025-03-22 22:02:56 +0000

    creating seperate files for menu and accessing the files

---
+++ GroupAssignment/pythonFiles/Demo_Python_Files/demo_Menu.py
@@ -0,0 +1,24 @@
+'''
+demo_Menu.py
+
+Author(s):  Wyatt Evans 1293147, Harveen Bajwa 1337280, Indigo Asher 1348315
+
+Project: Milestone III
+Date of Last Update: March 23th 2025
+
+Functional Summary
+    File implements a menu to the project, allowing for user accesability. 
+    Based on user decisions in the menu, call on 1 of 3 graph files and send which datafields they require.
+'''
+
+# import tkinter as tk
+# from tkinter import messagebox
+
+from tkinter import *
+
+from question1_Graph import *
+#from question2_Graph import *
+#from question3_Graph import *
+
+demo_Menu = Tk()
+demo_Menu.mainloop()
